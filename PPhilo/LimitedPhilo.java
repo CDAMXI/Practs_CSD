14de9f7ca496c009276f95cd4c378a801781a6aa
CDAMXI <cdachongm@gmail.com>
2025-03-04 14:58:11 +0100

    Refactor constructor formatting and improve code consistency across philosopher and table classes

--- PPhilo/LimitedPhilo.java
+++ PPhilo/LimitedPhilo.java
@@ -1,16 +1,26 @@
 // CSD Mar 2013 Juansa Sendra
 
 public class LimitedPhilo extends Philo {
-    public LimitedPhilo(int id, int cycles, int delay, Table table) {super(id, cycles, delay, table);}
-    public void run(){
-        try{
-            for(int i = 0; i < 4; i++){
-                table.takeL(id);delay();table.takeR(id);
-                table.eat(id); randomDelay();
-                table.dropR(id); delay(); table.dropL(id);
-                table.ponder(id); randomDelay();
+    public LimitedPhilo(int id, int cycles, int delay, Table table) {
+        super(id, cycles, delay, table);
+    }
+
+    public void run() {
+        try {
+            for (int i = 0; i < 4; i++) {
+                table.takeL(id);
+                delay();
+                table.takeR(id);
+                table.eat(id);
+                randomDelay();
+                table.dropR(id);
+                delay();
+                table.dropL(id);
+                table.ponder(id);
+                randomDelay();
             }
             table.end(id);
-        }catch(Exception e){}
+        } catch (Exception e) {
+        }
     }
 }
--- PPhilo/LimitedTable.java
+++ PPhilo/LimitedTable.java
@@ -1,5 +1,7 @@
 // CSD Mar 2013 Juansa Sendra
 
-public class LimitedTable extends RegularTable { //max 4 in dinning-room
-    public LimitedTable(StateManager state) {super(state);}
+public class LimitedTable extends RegularTable { //max 4 in dining-room
+    public LimitedTable(StateManager state) {
+        super(state);
+    }
 }
